54b35f274a900041156a510e269c4033af451d1e
Nag <nagcloudlab@gmail.com>
2024-05-23 15:39:03 +0530

    producer & consumer api

---
+++ kafka-clients/src/main/java/com/example/CustomProducerInterceptor.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Map;
+
+public class CustomProducerInterceptor implements org.apache.kafka.clients.producer.ProducerInterceptor<String, String> {
+
+    @Override
+    public ProducerRecord<String, String> onSend(ProducerRecord<String, String> record) {
+        //....
+        return record;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
+        //...
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
